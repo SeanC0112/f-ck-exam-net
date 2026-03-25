6a0f0068328f3acbed4b6b2dc05226a0eaefd934
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-03-24 23:12:44 -0700

    comment my code

--- fuckexamnet.py
+++ fuckexamnet.py
@@ -2,17 +2,22 @@ import keyboard as key
 import time, pyperclip
 from pynput import keyboard as kb
 
+#store whats on clipboard
 clipboard = ""
 
 def on_activate_i():
     global clipboard
+    #gets whats on the clipboard
     clipboard = pyperclip.paste()
     print('Clipboard content: ' + clipboard)
     time.sleep(1)
+    #writes to the keyboard
     key.write("hola")
 
 
+#add hotkey
 with kb.GlobalHotKeys({
+        #the keys you want.    the function you want to call
         '<cmd>+<shift>+<tab>': on_activate_i}) as h:
     h.join()
 
