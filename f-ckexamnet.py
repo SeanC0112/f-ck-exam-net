a4b4f6c1dc50554402941641d813f96ce2f168a9
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-03-24 23:51:20 -0700

    add lwhs

--- f-ckexamnet.py
+++ f-ckexamnet.py
@@ -2,7 +2,8 @@
 # dishonesty of any kind. I personally have not and never will use this software 
 # for cheating in examnet. I instead would like people to know about this vulnerability 
 # so that they are able to demonstrate to teachers or administrators that examnet 
-# isn't secure and shouldn't be used in class (specifically for urban high school).
+# isn't secure and shouldn't be used in class specifically for urban high school and 
+# possibly lwhs if i get around to it).
 
 import keyboard as key
 import time, pyperclip
