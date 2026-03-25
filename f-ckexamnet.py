01fbf3035299a879c1b366510f3c3d8f12c2145a
SeanC0112 <146582100+SeanC0112@users.noreply.github.com>
2026-03-25 06:16:48 -0700

    update disclaimer

--- f-ckexamnet.py
+++ f-ckexamnet.py
@@ -1,9 +1,9 @@
-# Disclaimer: I do not intend this piece of code to be used for cheating or academic
-# dishonesty of any kind. I personally have not and never will use this software 
+# Disclaimer: I do not intend nor support this piece of code to be used for cheating or
+# academic dishonesty of any kind. I personally have not and never will use this software 
 # for cheating in examnet. I instead would like people to know about this vulnerability 
-# so that they are able to demonstrate to teachers or administrators that examnet 
-# isn't secure and shouldn't be used in class specifically for urban high school and 
-# possibly lwhs if i get around to it).
+# so that they are able to demonstrate to teachers or administrators that examnet isn't 
+# secure and shouldn't be used in class specifically for urban high school and possibly
+# lwhs if i get around to it).
 
 import keyboard as key
 import time, pyperclip
